5a69158a68012bf21a40d5c79127103226c36173
ünsal pişirgen <unsalpisirgen1991@hotmail.com>
2017-07-06 22:29:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+** Project Name: AlperaFramework
+** Project Version: 1.0
+** Project Url:http://www.alperasoft.com
+** Project Developer:Ünsal İbrrahim Pişirgen
+*/
+/*
+   General script security provisioning
+*/
+    define('__SECURITY__',true);
+/*
+**
+    To call the file where system constants are
+**    
+*/ 
+    require_once "system/getFile.php";
+/*
+     System setting file
+*/
+    require_once _SYSTEM_."AF_Config.php";
+/*
+    
+*/    
+
+
+
+
+
